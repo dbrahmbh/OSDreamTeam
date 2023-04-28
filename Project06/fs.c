20132ada6573dec5793e944cc59f0d05af0235bb
agonza42 <agonza42@nd.edu>
2023-04-28 00:18:29 -0400

    Progress

--- Project06/fs.c
+++ Project06/fs.c
@@ -304,6 +304,97 @@ int fs_create()
 	// Create a new inode of zero length. On success, return the (positive) inumber. On
 	// failure, return zero. (Note that this implies zero cannot be a valid inumber.)
 
+	// Check if file system is not mounted
+	if (!isMounted) {
+
+		printf("Error: Filesystem not mounted\n");
+		return 0;
+
+	}
+
+	// Initialize block
+	union fs_block block;
+
+	// Read data from block
+	disk_read(thedisk, 0, block.data);
+
+	// Initialize inode blocks number and variables for iteration
+	int inodeblocksNum = block.super.ninodeblocks;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int inumber;
+
+	// Iterate through all the inodes
+	for (i = 1; i <= inodeblocksNum; i++) {
+
+		// Read the block data
+		disk_read(thedisk, i, block.data);
+
+		// Iterate through the inodes in a block
+		for (j = 0; j < INODES_PER_BLOCK; j++) {
+
+			// Check if the inode is valid
+			if (block.inode[j].isvalid == 0) {
+
+				// Update inode to invalid
+				block.inode[j].isvalid = 1;
+
+				// Use time function to get the time
+				time(&timeTemp);
+
+				// Update ctime and size for the inode
+				block.inode[j].ctime = timeTemp;
+				block.inode[j].size = 0;
+
+				// Initialize indirect block
+				union fs_block indirectBlock;
+
+				// Check if indirect blocks exist
+				if (block.inode[j].indirect > 0) {
+
+					// Read indirect block data
+					disk_read(thedisk, block.inode[j].indirect, indirectBlock.data);
+
+					// Iterate through pointers in a block
+					for (k = 0; k < POINTERS_PER_BLOCK; k++) {
+
+						// Check if there are indirect pointers and set to zero
+						if (indirectBlock.pointers[k] > 0) {
+							indirectBlock.pointers[k] = 0;
+						}
+						
+					}
+
+					// Write data to the disk
+					disk_write(thedisk, block.inode[j].indirect, indirectBlock.data);
+
+				}
+
+				// Iterate through the pointers in an inode
+				for (k = 0; k < POINTERS_PER_INODE; k++) {
+
+					// Set direct blocks to 0
+					block.inode[j].direct[k] = 0;
+
+				}
+
+				// Set indirect block to 0
+				block.inode[j].indirect = 0;
+				
+				// Write data onto the disk
+				disk_write(thedisk, i, block.data);
+
+				// Calculate inumber
+				inumber = (i - 1) * INODES_PER_BLOCK + j;
+
+				// Return inumber
+				return inumber;
+
+			}
+		}
+	}
+
 	return 0;
 
 }
@@ -316,7 +407,81 @@ int fs_delete( int inumber )
 	// assigned to this inode and return them to the free block map. On success, return one. On failure,
 	// return 0.
 
-	return 0;
+	// Check if filesystem is not mounted
+	if (!isMounted) {
+		printf("Error: Filesystem not mounted\n");
+		return 0;
+	}
+
+	// Initialize the block
+	union fs_block block;
+
+	// Read the block data
+	disk_read(thedisk, 0, block.data);
+
+	// Initialize inodes number, block number and offset, and variables for iteration
+	int inodesNum = block.super.ninodes;
+	int blockNum = (inumber - 1) / INODES_PER_BLOCK + 1;
+	int offset = (inumber - 1) % INODES_PER_BLOCK;
+	int i = 0;
+	int j = 0;
+
+	// Read the block data
+	disk_read(thedisk, blockNum, block.data);
+
+	// Initialize the indirect block
+	union fs_block indirectBlock;
+
+	// Check if number is valid
+	if (inumber > inodesNum || inumber < 1) {
+		printf("Error: Invalid number\n");
+		return 0;
+	}
+
+	// Check if the inode number exists
+	if (block.inode[offset].isvalid == 0) {
+		printf("Error: Inode number doesn't exist\n");
+		return 0;
+	}
+
+	// Set inodes to valid
+	block.inode[offset].isvalid = 0;
+
+	// Write data to the disk
+	disk_write(thedisk, blockNum, block.data);
+
+	// Iterate through pointers in the inode
+	for (i = 0; i < POINTERS_PER_INODE; i++) {
+		
+		// Check if direct blocks exist to delete
+		if (block.inode[offset].direct[i] > 0) {
+			freeBitmapBlock[block.inode[offset].direct[i]] = 0;
+		}
+
+	}
+
+	// Check if indirect blocks exist
+	if (block.inode[offset].indirect > 0) {
+
+		// Read indirect block data
+		disk_read(thedisk, block.inode[offset].indirect, indirectBlock.data);
+
+		// Iterate through pointers in the block
+		for (j = 0; j < POINTERS_PER_BLOCK; j++) {
+			
+			// Check if pointers exist to delete
+			if (indirectBlock.pointers[i] > 0) {
+				freeBitmapBlock[indirectBlock.pointers[i]] = 0;
+			}
+			
+		}
+
+		// Change indirect blocks to 0
+		freeBitmapBlock[block.inode[offset].indirect] = 0;
+
+	}
+
+	return 1;
 
 }
 
@@ -327,7 +492,40 @@ int fs_getsize( int inumber )
 	// Return the logical size of the given inode, in bytes. Note that zero is a valid logical
 	// size for an inode! On failure, return -1.
 
-	return 0;
+	// Check if filesystem is mounted
+	if (!isMounted) {
+		printf("Error: The fs must be already mounted to perform 'fs_getsize'\n");
+		return 0;
+	}
+
+	// Initialize block
+	union fs_block block;
+
+	// Read the block data
+	disk_read(thedisk, 0, block.data);
+
+	// Initialize inodes temp variable
+	int ninodesTemp = block.super.ninodes;
+
+	// Check if inode number is in bounds
+	if (inumber < 1 || inumber > ninodesTemp){
+		printf("Error: Inode number is out of bounds\n");
+		return -1;
+	}
+
+	// Initialize offset and blocknum
+	int blockNum = (inumber - 1) / (INODES_PER_BLOCK) + 1;
+	disk_read(thedisk, blockNum, block.data);
+	int maskOffset = (inumber - 1) % INODES_PER_BLOCK;
+
+	// Check if offset is valis
+	if (block.inode[maskOffset].isvalid == 0) {
+		printf("Error: Given inode does not seem to exist\n");
+		return -1;
+	}
+
+	// Return logical size of given inode in bytes
+	return block.inode[maskOffset].size;
 
 }
 
@@ -340,7 +538,136 @@ int fs_read( int inumber, char *data, int length, int offset )
 	// bytes actually read could be smaller than the number of bytes requested, perhaps if the end of
 	// the inode is reached. If the given inumber is invalid, or any other error is encountered, return 0.
 
-	return 0;
+	// Check if file system is mounted
+	if (!isMounted) {
+        printf("Error: Filesystem not mounted\n");
+        return 0;
+    }
+
+	// Initialize block and read data
+    union fs_block block;
+    disk_read(thedisk, 0, block.data);
+
+	// Initialize inodes number, block number, and offset before reading data
+    int inodesNum = block.super.ninodes;
+    int blockNum = (inumber - 1) / INODES_PER_BLOCK + 1;
+    int offsetI = (inumber - 1) % INODES_PER_BLOCK;
+    disk_read(thedisk, blockNum, block.data);
+
+	// Initialize variables for iteration
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int l = 0;
+
+	// Initialize inode block
+    union fs_block inodeBlock;
+
+	// Initialize bytes and head and curr counters to track
+    int bytesR = 0;
+    int headB = offset / BLOCK_SIZE;
+    int headO = offset % BLOCK_SIZE;
+    int currB = 0;
+    int currO = 0;
+
+	// Initialize indirect block
+    union fs_block indirectBlock;
+
+	// Check if the number is invalid
+    if (inumber > inodesNum || inumber < 1) {
+        printf("Error: Invalid number\n");
+        return 0;
+    }
+
+	// Check if the inode number exists
+    if (block.inode[offsetI].isvalid == 0) {
+        printf("Error: Inode number doesn't exist\n");
+        return 0;
+    }
+
+	// Iterate through the pointers in an inode
+    for (i = 0; i < POINTERS_PER_INODE; i++) {
+
+		// Check if direct blocks exist
+        if (block.inode[offsetI].direct[i] > 0) {
+
+			// Check if head is greater than the current to update the current
+            if (currB < headB) {
+                currB++;
+                continue;
+            }
+
+			// Read data from the inode block
+            disk_read(thedisk, block.inode[offsetI].direct[i], inodeBlock.data);
+
+			// Iterate through the block
+            for (j = 0; j < BLOCK_SIZE; j++) {
+
+				// Return the bytes if its greater than the length
+                if (bytesR >= length) {
+                    return bytesR;
+                }
+
+				// If the head is greater than the current, update current
+                if (currO < headO) {
+                    currO++;
+                    continue;
+                }
+
+				// Save the data and update the bytes
+                data[bytesR] = inodeBlock.data[j];
+                bytesR++;
+
+            }
+        }
+    }
+
+	// Check if indirect blocks exist
+    if (block.inode[offsetI].indirect > 0) {
+
+		// Read indirect block data
+        disk_read(thedisk, block.inode[offsetI].indirect, indirectBlock.data);
+
+		// Iterate through the pointers in a block
+        for (k = 0; k < POINTERS_PER_BLOCK; k++) {
+
+			// Check if pointers exist
+            if (indirectBlock.pointers[k] > 0) {
+
+				// If head is greater than current, update current
+                if (currB < headB) {
+                    currB++;
+                    continue;
+                }
+
+				// Read data from inode block
+                disk_read(thedisk, indirectBlock.pointers[k], inodeBlock.data);
+
+				// Iterate through the block
+                for (l = 0; l < BLOCK_SIZE; l++) {
+
+					// Check if the bytes are greater than or equal to length, if so, return bytes
+                    if (bytesR >= length) {
+                        return bytesR;
+                    }
+
+					// If head is greater than current, update current
+                    if (currO < headO) {
+                        currO++;
+                        continue;
+                    }
+
+					// Save the data and update the bytes
+                    data[bytesR] = inodeBlock.data[l];
+                    bytesR++;
+					
+                }
+            }
+        }
+    }
+
+	// Return bytes
+	return bytesR;
 
 }
 
@@ -355,5 +682,5 @@ int fs_write( int inumber, const char *data, int length, int offset )
 	// any other error is encountered, return 0.
 
 	return 0;
-
+	
 }
